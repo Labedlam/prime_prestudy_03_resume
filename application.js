c0a1f0bd40e8f30b606c08e09ab0766e93e21450
Labedlam <Zeo@Zeeshans-MacBook-Pro.local>
2015-12-15 07:48:19 -0600

    Update Resume with jquery to toggle info when clicked and added hover function

--- application.js
+++ application.js
@@ -2,33 +2,44 @@ jQuery(document).ready(function(){
  console.log("hello")
 
 	
-	//$('.flexbox').find(li);
-		$('.flexbox').find('li').hide();
-//$('.flexbox').toggle();
-$('.Eclass1').on('click', function(){   // When button is clicked all conent is shown
-	//$(this).addClass('.hidden');
-	//$(this).css('background','purple');
-
-	$(this).find('li').slideToggle();
-	 console.log( "homie come on now" );
+$('.flexbox').find('li').hide();
+$('.flexbox').hide();
+slideAccomplishedTasks();
+slideExperience();
+hoverOver();
+hoverOverDetail();
 });
 
 
+function slideAccomplishedTasks(){
+	$('.Eclass1').on('click', function(){   
+		$(this).find('li').slideToggle();
+	 	console.log( "homie come on now" );
 
-$('.flexbox').on('click', 'li',function(){
-	$('li').toggle();
-});
+	});
+}
 
+function slideExperience(){
+	$('.button').on('click',function(){
+		$(this).nextUntil('.button').slideToggle();
+		console.log("hey there");
+	});
+}// im running into the problem where only one of my experiences is toggling 
+//I need to find a way to call only a select few elements 
+// I could maybe use filter if I add a specific class
 
+function hoverOver(){
+	$('.button').hover(function(){
+		$(this).toggleClass("highlight");
+		console.log("im suppose to highlight");
+	});	
 
-//$(this).addClass('.hidden');
-//$(this).css('visibility','visible');
-	//$(this).css('background','purple');
-	//$(this).find('ul').css('visibility','visible');
+};// couldnt understand why the color of the paragraph wont change
 
+function hoverOverDetail(){
+	$('.Eclass1').hover(function(){
+		$(this).toggleClass("highlight");
+		console.log("im suppose to highlight again");
+	});	
 
-	 //console.log( "homie come on now" );
-
-
-
-});
+};
